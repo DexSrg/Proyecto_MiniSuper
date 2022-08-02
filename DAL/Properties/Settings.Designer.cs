d9f984800ec2679286439e557d00a7da9b6a700a
Marvin Mejia <SrgMejia@icloud.com>
2022-08-01 21:30:09 -0600

    Quitando cadena de conexion de las configuraciones de la dal.

--- DAL/Properties/Settings.Designer.cs
+++ DAL/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace DAL.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SRG\\SQL2017;Initial Catalog=MiniSuper;Integrated Security=True")]
-        public string Conexion {
-            get {
-                return ((string)(this["Conexion"]));
-            }
-        }
     }
 }
